07c2fdf97580dc15e58897e8d3b0ef2a594164b8
Alberto-Izquierdo <alberto.izquierdo.ramirez@gmail.com>
2020-04-01 19:03:10 +0200

    Added bash_profile.

--- setup.sh
+++ setup.sh
@@ -5,10 +5,12 @@ mkdir ./backup
 mv ~/.bashrc ./backup/
 mv ~/.gitignore ./backup/
 mv ~/.tmux.conf ./backup/
+mv ~/.bash_profile ./backup/
 sudo mv /usr/share/X11/xorg.conf.d/10-monitor.conf ./backup/
 
 #copy the new one
 cp ./.bashrc ~
 cp ./.gitignore ~
 cp ./.tmux.conf ~
+cp ./.bash_profile.conf ~
 sudo cp ./10-monitor.conf /usr/share/X11/xorg.conf.d/
