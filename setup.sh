8e8d6fe7ae5b43df94ed8fbfddb85d76b0d0e942
Alberto-Izquierdo <alberto.izquierdo.ramirez@gmail.com>
2021-02-15 20:42:43 +0100

    Backup files are now copied in a different directory each time the setup script is executed

--- setup.sh
+++ setup.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-mkdir ./backup
+
+time_var=$(date +"%F_%T")
+directory_var="./backups/${time_var}/"
+
+mkdir -p ${directory_var}
 
 #backup the previous configuration
-mv ~/.bashrc ./backup/
-mv ~/.gitignore ./backup/
-mv ~/.tmux.conf ./backup/
-mv ~/.bash_profile ./backup/
-sudo mv /usr/share/X11/xorg.conf.d/10-monitor.conf ./backup/
+mv ~/.bashrc ${directory_var}
+mv ~/.gitignore ${directory_var}
+mv ~/.tmux.conf ${directory_var}
+mv ~/.bash_profile ${directory_var}
+#sudo mv /usr/share/X11/xorg.conf.d/10-monitor.conf ./backup/
 
 #copy the new one
 cp ./.bashrc ~
 cp ./.gitignore ~
 cp ./.tmux.conf ~
 cp ./.bash_profile ~
-sudo cp ./10-monitor.conf /usr/share/X11/xorg.conf.d/
+#sudo cp ./10-monitor.conf /usr/share/X11/xorg.conf.d/
