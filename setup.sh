3621ab5de238495541987dd8a6f3e2489c055709
Alberto-Izquierdo <alberto.izquierdo.ramirez@gmail.com>
2021-02-15 20:52:09 +0100

    Added i3 configuration

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 time_var=$(date +"%F_%T")
-directory_var="./backups/${time_var}/"
+backup_directory="./backups/${time_var}/"
 
-mkdir -p ${directory_var}
+mkdir -p ${backup_directory}
 
 #backup the previous configuration
-mv ~/.bashrc ${directory_var}
-mv ~/.gitignore ${directory_var}
-mv ~/.tmux.conf ${directory_var}
-mv ~/.bash_profile ${directory_var}
+cp ~/.bashrc ${backup_directory}
+cp ~/.gitignore ${backup_directory}
+cp ~/.tmux.conf ${backup_directory}
+cp ~/.bash_profile ${backup_directory}
+cp ~/.config/i3/config ${backup_directory}/i3_config
 #sudo mv /usr/share/X11/xorg.conf.d/10-monitor.conf ./backup/
 
 #copy the new one
@@ -17,4 +18,5 @@ cp ./.bashrc ~
 cp ./.gitignore ~
 cp ./.tmux.conf ~
 cp ./.bash_profile ~
+cp -r .config ~
 #sudo cp ./10-monitor.conf /usr/share/X11/xorg.conf.d/
