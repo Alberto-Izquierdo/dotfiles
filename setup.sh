d3842bc2e835b9e951f0d1d696c8371cc4c4cea3
Alberto <alberto@pop-os.localdomain>
2021-06-18 20:39:03 +0200

    Added gitconfig file

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ cp ~/.gitignore ${backup_directory}
 cp ~/.tmux.conf ${backup_directory}
 cp ~/.bash_profile ${backup_directory}
 cp ~/.config/i3/config ${backup_directory}/i3_config
+cp ~/.gitconfig ${backup_directory}
 #sudo mv /usr/share/X11/xorg.conf.d/10-monitor.conf ./backup/
 
 #copy the new one
@@ -18,5 +19,6 @@ cp ./.bashrc ~
 cp ./.gitignore ~
 cp ./.tmux.conf ~
 cp ./.bash_profile ~
+cp ./.gitconfig ~
 cp -r .config ~
 #sudo cp ./10-monitor.conf /usr/share/X11/xorg.conf.d/
