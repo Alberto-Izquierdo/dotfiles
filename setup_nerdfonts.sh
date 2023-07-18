b7282561caa94f8e1db726aaf0b9ef94a0b461ae
Alberto Izquierdo <a.izquierdo@zaleos.net>
2023-07-18 20:06:41 +0200

    Added script to install nerd-fonts

---
+++ setup_nerdfonts.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+pushd /tmp
+echo "Cloning nerd-fonts repository..."
+git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git
+cd nerd-fonts
+echo "Installing nerd-fonts ..."
+./install.sh
+cd ..
+rm -rf nerd-fonts
+echo "Nerd-fonts installed!
+popd
